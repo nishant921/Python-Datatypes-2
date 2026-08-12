1191e563a7b4b29cd512a06e2094ca62f517cdf1
NISHANT CHAURASIA <nishankumarji@gmail.com>
2026-08-12 19:53:10 +0530

    tuples in python

---
+++ operationsintuple.py
@@ -0,0 +1,15 @@
+# operations on tuple
+# + and *
+
+t1=(1,2,3,4)
+t2=(4,5,56,7)
+print(t1+t2)
+print(t1*3)
+
+# membership function
+print(t1 in t2)
+print(4 in t2)
+
+# iteration
+for item in t1:
+    print(item)
